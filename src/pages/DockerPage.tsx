776556b94abd68a8d5a4c3b2636a2dd55d14b242
zchary <zcharyma@gmail.com>
2022-05-20 20:29:13 +0800

    add action button

--- src/pages/DockerPage.tsx
+++ src/pages/DockerPage.tsx
@@ -1,13 +1,36 @@
+import { Button, Divider, Drawer, Space } from "@mantine/core";
+import { useState } from "react";
+import { Plus } from "tabler-icons-react";
 import ContainerList from "../components/ContainerList";
 import AppContent from "../layout/Content";
 
 export default function DockerPage() {
+  const [opened, setOpened] = useState(false);
+
   return (
     <>
       <AppContent>
         <h1>Docker Management</h1>
+        <Button variant="outline" compact onClick={() => setOpened(true)} leftIcon={<Plus size={16} />}>
+          Create Container
+        </Button>
+        <Space h="xs" />
+        <Divider />
         <ContainerList />
+        <Drawer
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title="Register"
+          padding="xl"
+          size="xl"
+          position="right"
+          transition="rotate-left"
+          transitionDuration={250}
+          transitionTimingFunction="ease"
+        ></Drawer>
       </AppContent>
     </>
   );
 }
+
+const CreateContainerWindow = () => {};
